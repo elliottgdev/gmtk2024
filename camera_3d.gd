a96505b89ed961c79fea774519e2bbaa7a805eb9
elliottgdev <146915307+elliottgdev@users.noreply.github.com>
2024-08-19 21:57:33 +1000

    new level

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,11 +1,13 @@
 extends Camera3D
 
 var player_node
+var y = 0;
 
 func _ready() -> void:
 	#find player node // this runs on start
 	player_node = get_node("../player")
+	y = player_node.get_global_position().y
 
 func _physics_process(delta: float) -> void:
 	#set position to player node with an offset
-	global_position = player_node.get_global_position() + Vector3(0, 11, 6)
+	global_position = Vector3(player_node.get_global_position().x, y + 6, player_node.get_global_position().z + 8)
--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
-@export var speed = 20
-@export var gravity = 75
-@export var jump  = 30
+@export var speed = 6
+@export var gravity = 30
+@export var jump  = 10
 @export var debug = false
 var is_in_scale_zone = false
 var target_velocity = Vector3.ZERO
@@ -16,33 +16,33 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		if !$Node3D/Footstepsound.playing and is_on_floor():
-			$Node3D/Footstepsound.pitch_scale = (scale.y * 1.25)
+			$Node3D/Footstepsound.pitch_scale = (1.25 / scale.y)
 			$Node3D/Footstepsound.play()
 	if Input.is_action_pressed("move_backward"):
 		direction.z += 1
 		if !$Node3D/Footstepsound.playing and is_on_floor():
-			$Node3D/Footstepsound.pitch_scale = (scale.y * 1.25)
+			$Node3D/Footstepsound.pitch_scale = (1.25 / scale.y)
 			$Node3D/Footstepsound.play()
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 		if !$Node3D/Footstepsound.playing and is_on_floor():
-			$Node3D/Footstepsound.pitch_scale = (scale.y * 1.25)
+			$Node3D/Footstepsound.pitch_scale = (1.25 / scale.y)
 			$Node3D/Footstepsound.play()
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 		if !$Node3D/Footstepsound.playing and is_on_floor():
-			$Node3D/Footstepsound.pitch_scale = (scale.y * 1.25)
+			$Node3D/Footstepsound.pitch_scale = (1.25 /scale.y)
 			$Node3D/Footstepsound.play()
 		
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			target_velocity.y = jump * basis.get_scale().y
-			$Node3D/Jumpsound.pitch_scale = 1
+			$Node3D/Jumpsound.pitch_scale = 0.75
 			$Node3D/Jumpsound.play()
 		elif double_jump == true:
 			target_velocity.y = jump * basis.get_scale().y
 			double_jump = false
-			$Node3D/Jumpsound.pitch_scale = 2
+			$Node3D/Jumpsound.pitch_scale = 1.25
 			$Node3D/Jumpsound.play()
 	#debug input. only works if debug mode is enabled on player in inspector
 	if debug == true or is_in_scale_zone == true:
@@ -63,8 +63,14 @@ func _physics_process(_delta: float) -> void:
 	
 	if not is_on_floor():
 		#gravity
-		target_velocity.y -= gravity * basis.get_scale().y * _delta
+		if basis.get_scale().y < 0.7:
+			target_velocity.y -= (gravity - 7) * basis.get_scale().y * _delta
+		else:
+			target_velocity.y -= gravity * basis.get_scale().y * _delta
 	else: double_jump = true #set double jump to true if player is on the ground
+	
+	if is_on_ceiling():
+		target_velocity.y = -0.1
 	
 	#move
 	velocity = target_velocity
@@ -79,6 +85,11 @@ func _on_area_3d_area_entered(area: Area3D) -> void:
 	speed = 0
 	jump = 0
 	$Node3D/Lavadeath.play()
+	#$Node3D/Sizzle.pitch_scale = 0.6
+	$Node3D/HitHurt.play()
+	$Node3D/Sizzle.play()
+	$Node3D/Scream.volume_db = 0.5
+	$Node3D/Scream.pitch_scale = 0.9
 	$Node3D/Scream.play()
 	await get_tree().create_timer(1).timeout
 	axis_lock_linear_x = true
